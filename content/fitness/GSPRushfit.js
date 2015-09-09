92ec491d692c2acfbd616e468aaa0aa9abd76395
aconyteds <ianwilson1337@gmail.com>
2015-09-09 23:44:22 +0000

    Update content Files.

---
+++ content/fitness/GSPRushfit.js
@@ -0,0 +1,15 @@
+define(function(){
+     return {
+          pages:[{
+               title:"GSP Rushfit Overview and Review",
+               hash:"Fitness/GSPRushfit",
+               content:[{
+                    "$type":"text",
+                    "innerHTML":"GSP Rushfit is an 8 week program which is designed to help you improve your performance by performing move inspired by the UFC. The trainer, Erik Owings, is a former MMA fighter. He is the personal trainer for Georges St. Pierre who is featured throughout each of the videos. The trainer explains each move with enough detail to allow you to accomplish them while not looking at the screen (which is great). While you are working out, he also provides extra technical details about the moves to help make sure that you are keeping good form and recieving the greatest benefit from performing them. Along with Erik and GSP, you also get two more participatns who usually show you a modifier or simply an alternative way to do each move. "
+               },{
+                    "$type":"text",
+                    "innerHTML":"The series itself consists of both traditional and non-traditional moves. The focus does seem to be on leg exercises, but there is enough to build muscle "
+               }]
+          }]
+     }
+});
